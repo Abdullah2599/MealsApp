5419f653dc58097e8b31d964347f09b19a7ad114
Abdullah2599 <abdullahcloudupload@gmail.com>
2024-05-24 13:14:39 +0500

    1

---
+++ meals_app/lib/screens/categories.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Categories extends StatelessWidget {
+  const Categories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pick your Category"),
+      ),
+      body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+            childAspectRatio: 3 / 2,
+          ),
+          itemBuilder: itemBuilder),
+    );
+  }
+}
