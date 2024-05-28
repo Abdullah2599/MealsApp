da5e01a582727b764d0cfe0309bca75491fab2e6
Abdullah2599 <abdullahcloudupload@gmail.com>
2024-05-29 00:30:57 +0500

    drawer

--- meals_app/lib/screens/tabs.dart
+++ meals_app/lib/screens/tabs.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -14,6 +15,15 @@ class _TabsScreenState extends State<TabsScreen> {
   int _selectPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
 
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
   void _toggleMealFavoriteStatus(Meal meal) {
     final isExisting = _favoriteMeals.contains(meal);
 
@@ -21,11 +31,13 @@ class _TabsScreenState extends State<TabsScreen> {
       setState(() {
         _favoriteMeals.remove(meal);
       });
+      _showInfoMessage('Meal is no longer a favorite');
     } //pendiong
     else {
       setState(() {
         _favoriteMeals.add(meal);
       });
+      _showInfoMessage('Marked as Favorite');
     }
   }
 
@@ -44,13 +56,14 @@ class _TabsScreenState extends State<TabsScreen> {
 
     if (_selectPageIndex == 1) {
       activePage = MealsScreen(
-        meals: [],
+        meals: _favoriteMeals,
         onToggleFavorite: _toggleMealFavoriteStatus,
       );
       activePageTitle = 'Your Favorites';
     }
 
     return Scaffold(
+      drawer: const MainDrawer(),
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
